5b6646c2625cf62ec50bf6605508d6be6c408857
jjcopson <jeffreycopson13@gmail.com>
2026-08-29 00:04:53 +0000

    Fix build.sh: replace wget with curl for Vercel environment

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-wget -q https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.32.2-stable.tar.xz -O /tmp/flutter.tar.xz
+curl -sL https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.32.2-stable.tar.xz -o /tmp/flutter.tar.xz
 tar xf /tmp/flutter.tar.xz -C $HOME
 export PATH=$PATH:$HOME/flutter/bin
 flutter config --no-analytics --enable-web
