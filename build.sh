b78f96bea043dd9c09b63db501576dbd8b1940f9
jjcopson <jeffreycopson13@gmail.com>
2026-08-29 00:32:36 +0000

    Fix build: add git safe.directory for Flutter root ownership issue

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 set -e
 curl -sL https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.32.2-stable.tar.xz -o /tmp/flutter.tar.xz
 tar xf /tmp/flutter.tar.xz -C /tmp
+git config --global --add safe.directory /tmp/flutter
+git config --global --add safe.directory '*'
+export FLUTTER_ROOT=/tmp/flutter
 /tmp/flutter/bin/flutter config --no-analytics --enable-web
 /tmp/flutter/bin/flutter pub get
 /tmp/flutter/bin/flutter build web --release --base-href /
