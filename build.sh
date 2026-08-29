8bc37b231c9308011b3200a988ea1ca2bdc5cf6c
jjcopson <jeffreycopson13@gmail.com>
2026-08-29 00:20:42 +0000

    Fix Vercel: use bash build.sh, extract Flutter to /tmp, fix executable bit

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -e
 curl -sL https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.32.2-stable.tar.xz -o /tmp/flutter.tar.xz
-tar xf /tmp/flutter.tar.xz -C $HOME
-export PATH=$PATH:$HOME/flutter/bin
-flutter config --no-analytics --enable-web
-flutter pub get
-flutter build web --release --base-href /
+tar xf /tmp/flutter.tar.xz -C /tmp
+/tmp/flutter/bin/flutter config --no-analytics --enable-web
+/tmp/flutter/bin/flutter pub get
+/tmp/flutter/bin/flutter build web --release --base-href /
